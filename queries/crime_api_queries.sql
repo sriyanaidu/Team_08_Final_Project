04da667d7fb618a11612928253613631e2cb36cb
aryamanp1248 <160172904+aryamanp1248@users.noreply.github.com>
2024-05-14 18:16:20 -0400

    Update and rename crime_api_queries to crime_api_queries.sql

--- queries/crime_api_queries.sql
+++ queries/crime_api_queries.sql
@@ -1,5 +1,5 @@
-# CRIME API QUERIES
-# CRIME AGAINST PROPERTY IN SILVER SPRING IN 2022 to 2024 #
+-- CRIME API QUERIES
+-- CRIME AGAINST PROPERTY IN SILVER SPRING IN 2022 to 2024 #
 SELECT * FROM umd-inst767.montgomery_datasets.crimes
 WHERE crimename1='Crime Against Person'
 AND district = 'SILVER SPRING'
@@ -7,7 +7,7 @@ AND date BETWEEN '2023-01-01' AND '2023-12-31'
 ORDER BY date ASC;
 
 
-# TOP 10 MOST COMMON OFFENCES
+-- TOP 10 MOST COMMON OFFENCES
 SELECT offence_code, crimename1, crimename2,
   crimename3, COUNT(*) AS offence_count 
 FROM umd-inst767.montgomery_datasets.crimes 
@@ -16,7 +16,7 @@ GROUP BY offence_code, crimename1, crimename2,crimename3
 ORDER BY offence_count DESC 
 LIMIT 10;
 
-# ADDRESS STREET WITH MOST OFFENCES
+-- ADDRESS STREET WITH MOST OFFENCES
 SELECT 
   address_street,city,
   COUNT(*) AS offence_count
@@ -30,7 +30,7 @@ ORDER BY
 LIMIT 
   10;
 
-# CITY WITH MOST OFFENCES 
+-- CITY WITH MOST OFFENCES 
 SELECT 
   city,
   COUNT(*) AS offence_count
@@ -44,7 +44,7 @@ ORDER BY
 LIMIT 
   10;
 
-# CRIMES WITH MORE THAN 1 VICTIM INVOLVED
+-- CRIMES WITH MORE THAN 1 VICTIM INVOLVED
 SELECT 
   incident_id,
   crimename1,
